db43f95cce6d5a61dcef0e907774df50ffc82c3f
Artem Kovera <artemkovera@gmail.com>
2022-07-14 18:11:10 +0300

    more integer arithmetic

--- basic_code/integer_arithmetic.asm
+++ basic_code/integer_arithmetic.asm
@@ -10,8 +10,12 @@ section .data
     
     string_1 db "10 + 10 = %d",10,0
     string_2 db "20 - 10 = %d",10,0
+    string_3 db "10 * 10 = %d",10,0
+    string_4 db "100/10  = %d",10,0
 
 section .bss
+
+    result resq 1
 
 section .text
     
@@ -20,27 +24,56 @@ global main
 main:
 
     mov rbx, 10
-    mov rsi, 10
-    add rsi, rbx ;  10 + 10 
+    mov qword [result], 10
+    add qword [result], rbx ;  10 + 10 
     
     push rbp
     mov  rbp, rsp
     mov  rdi, string_1
+    mov  rsi, qword [result]
     mov  rax, XMM_REGISTERS_USED
     call printf    
     mov rsp, rbp
     pop rbp
     
-    mov rsi, 20
-    sub rsi, rbx ; 20 - 10 
+    
+    sub qword [result], rbx ; 20 - 10 
 
     push rbp
     mov  rbp, rsp
     mov  rdi, string_2
+    mov  rsi, qword [result]
     mov  rax, XMM_REGISTERS_USED
     call printf    
     mov rsp, rbp
-    pop rbp    
+    pop rbp 
+
+    mov rax, qword [result]
+    mul rbx                   ; 10 * 10
+    mov qword [result], rax
+
+    push rbp
+    mov  rbp, rsp
+    mov  rdi, string_3
+    mov  rsi, qword [result]
+    mov  rax, XMM_REGISTERS_USED
+    call printf    
+    mov rsp, rbp
+    pop rbp 
+
+    mov rax, qword [result]
+    mov ebx, 10
+    div ebx
+    mov qword [result], rax    
+
+    push rbp
+    mov  rbp, rsp
+    mov  rdi, string_4
+    mov  rsi, qword [result]
+    mov  rax, XMM_REGISTERS_USED
+    call printf    
+    mov rsp, rbp
+    pop rbp       
     
     mov rax, SYS_EXIT
     mov rdi, EXIT_SUCCESS
